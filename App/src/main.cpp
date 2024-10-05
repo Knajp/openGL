9288ad2e13ef2aa8621522063c94029d50a74378
unknown <jcfhjcftug@gmail.com>
2024-10-05 20:13:29 +0200

    idk

--- App/src/main.cpp
+++ App/src/main.cpp
@@ -3,6 +3,24 @@
 
 #include <iostream>
 
+
+const char* vertexShaderSource, fragShaderSource;
+
+GLfloat vertices[] =
+{
+    -0.5f, -0.5f, 0.0f,
+     0.0f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.25f, 0.0f, 0.0f,
+    -0.25f, 0.0f, 0.0f,
+     0.0f,  0.5f, 0.0f
+};
+GLuint indices[] =
+{
+    0,  4,  1,
+    1,  2,  3,
+    3,  4,  5
+};
 int main(int argc, char** argv)
 {
     glfwInit();
@@ -26,10 +44,32 @@ int main(int argc, char** argv)
     }   
     glViewport(0, 0, 800, 600);
 
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    GLuint EBO;
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_VERTEX_ARRAY, VBO);
+    glBufferData(GL_VERTEX_ARRAY, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices , GL_STATIC_DRAW);
+
     while (!glfwWindowShouldClose(window))
     {
         glfwSwapBuffers(window);
         glfwPollEvents();
+
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBindBuffer(GL_VERTEX_ARRAY, VBO);
+
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, indices);
     }
 
     return 0;
